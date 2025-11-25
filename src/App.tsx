b380ef39463bcc26dd8a44f055119e649627fa12
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-11-24 22:41:25 -0300

    Creating an Icon Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import Text from "./components/text"
+import Text from "./components/text";
+import Icon from "./components/icon";
+
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
+import CheckIcon from "./assets/icons/Check-Regular.svg?react";
+import PencilIcon from "./assets/icons/PencilSimple-Regular.svg?react";
+import PlusIcon from "./assets/icons/Plus-Regular.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import xIcon from "./assets/icons/X-Regular.svg?react";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-2">
-      <Text variant="body-sm-bold" className="text-pink-base">
-        Hello World!
-      </Text>
-      <Text className="text-green-base">
-        Hello World!
-      </Text>
-      <Text>
-        Levar o dog para passear!
-      </Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Hello World!
+        </Text>
+        <Text className="text-green-base">
+          Hello World!
+        </Text>
+        <Text>
+          Levar o dog para passear!
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={PencilIcon} />
+        <Icon svg={xIcon} />
+      </div>
     </div>
   )
 }
