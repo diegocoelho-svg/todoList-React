a48ac5d9d54923cec3f66bbf2d964104f5cc346c
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-11-27 19:40:06 -0300

    Creating an Input Text Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
+import InputText from "./components/input-text";
 
 import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
 import CheckIcon from "./assets/icons/Check-Regular.svg?react";
@@ -13,7 +14,7 @@ import xIcon from "./assets/icons/X-Regular.svg?react";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
+    <div className="grid gap-10">
       <div className="flex flex-col gap-2">
         <Text variant="body-sm-bold" className="text-pink-base">
           Hello World!
@@ -49,6 +50,10 @@ export default function App() {
         <ButtonIcon icon={TrashIcon} />
         <ButtonIcon icon={TrashIcon} variant="secondary" />
         <ButtonIcon icon={TrashIcon} variant="tertiary" />
+      </div>
+
+      <div>
+        <InputText />
       </div>
 
     </div>
