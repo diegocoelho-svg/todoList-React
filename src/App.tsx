f2533a735e2cdb2e473eb7a9e8fab6f261261088
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-11-27 21:16:17 -0300

    Creating an Input Checkbox Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Badge from "./components/badge";
 import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
+import InputCheckbox from "./components/input-checkbox";
 
 import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
 import CheckIcon from "./assets/icons/Check-Regular.svg?react";
@@ -54,6 +55,10 @@ export default function App() {
 
       <div>
         <InputText />
+      </div>
+
+      <div>
+        <InputCheckbox />
       </div>
 
     </div>
