858378ce4fc4eba0bc9ff9410737d99fcf530417
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-11-27 21:23:24 -0300

    Creating Card component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
 import InputCheckbox from "./components/input-checkbox";
+import Card from "./components/card";
 
 import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
 import CheckIcon from "./assets/icons/Check-Regular.svg?react";
@@ -59,6 +60,10 @@ export default function App() {
 
       <div>
         <InputCheckbox />
+      </div>
+
+      <div>
+        <Card size="md">Hello word!</Card>
       </div>
 
     </div>
