6fc35a1acf0967be20bfbb611af6e3da503915c7
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-12-01 18:39:06 -0300

    feat: implement layout structure and routing with React Router

---
+++ src/pages/page-components.tsx
@@ -0,0 +1,83 @@
+import Text from "../components/text";
+import Icon from "../components/icon";
+import Badge from "../components/badge";
+import Button from "../components/button";
+import ButtonIcon from "../components/button-icon";
+import InputText from "../components/input-text";
+import InputCheckbox from "../components/input-checkbox";
+import Card from "../components/card";
+
+import TrashIcon from "../assets/icons/Trash-Regular.svg?react";
+import CheckIcon from "../assets/icons/Check-Regular.svg?react";
+import PencilIcon from "../assets/icons/PencilSimple-Regular.svg?react";
+import PlusIcon from "../assets/icons/Plus-Regular.svg?react";
+import SpinnerIcon from "../assets/icons/spinner.svg?react";
+import xIcon from "../assets/icons/X-Regular.svg?react";
+import Container from "../components/container";
+import Skeleton from "../components/skeleton";
+
+export default function PageComponents() {
+  return (
+    <Container>
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-2">
+          <Text variant="body-sm-bold" className="text-pink-base">
+            Hello World!
+          </Text>
+          <Text className="text-green-base">
+            Hello World!
+          </Text>
+          <Text>
+            Levar o dog para passear!
+          </Text>
+        </div>
+
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-green-base" />
+          <Icon svg={CheckIcon} />
+          <Icon svg={PencilIcon} />
+          <Icon svg={PlusIcon} />
+          <Icon svg={SpinnerIcon} animate />
+          <Icon svg={PencilIcon} />
+          <Icon svg={xIcon} />
+        </div>
+
+        <div className="flex gap-1">
+          <Badge variant="secondary">5</Badge>
+          <Badge variant="primary">2 de 5</Badge>
+          <Badge isLoading>5</Badge>
+        </div>
+
+        <div>
+          <Button icon={PlusIcon}>Nova tarefa!</Button>
+        </div>
+
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secondary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+          <ButtonIcon icon={TrashIcon} isLoading />
+        </div>
+
+        <div>
+          <InputText />
+        </div>
+
+        <div>
+          <InputCheckbox />
+          <InputCheckbox isLoading />
+        </div>
+
+        <div>
+          <Card size="md">Hello word!</Card>
+        </div>
+
+        <div className="space-y-2">
+          <Skeleton className="h-6" />
+          <Skeleton className="h-6" />
+          <Skeleton className="w-96 h-6" />
+        </div>
+      </div>
+    </Container>
+  )
+}
---
+++ src/pages/page-home.tsx
@@ -0,0 +1,6 @@
+import Container from "../components/container";
+
+
+export default function PageHome() {
+  return <Container>Página Home!</Container>
+}
