ec318a960646eb9dec907834f1e6825116affe9a
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-12-03 19:32:41 -0300

    feat: implement task deletion using deleteTask, update UI and localStorage, and handle state when deleting tasks in edit mode

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -23,13 +23,17 @@ export default function TaskItem({ task }: TaskItemProps) {
   )
 
   const [taskTitle, setTaskTitle] = React.useState(task.title || "");
-  const { updateTask, updateTaskStatus } = useTask()
+  const { updateTask, updateTaskStatus, deleteTask } = useTask()
 
   function handleEditTask() {
     setIsEditing(true)
   }
 
   function handleExitEditTask() {
+    if (task.state === TaskState.Creating) {
+      deleteTask(task.id)
+    }
+
     setIsEditing(false)
   }
 
@@ -52,6 +56,10 @@ export default function TaskItem({ task }: TaskItemProps) {
     updateTaskStatus(task.id, checked)
   }
 
+  function handleClickDeleteTask() {
+    deleteTask(task.id)
+  }
+
   return (
     <Card size="md">
       {!isEditing ? (
@@ -71,6 +79,7 @@ export default function TaskItem({ task }: TaskItemProps) {
               type="button"
               icon={TrashIcon}
               variant="tertiary"
+              onClick={handleClickDeleteTask}
             />
             <ButtonIcon
               type="button"
